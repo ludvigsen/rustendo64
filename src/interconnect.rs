e4f06b87f53c5875878f82acade115183958523b
Marius Skaar Ludvigsen <marius.ludvigsen@gmail.com>
2016-06-06 23:32:28 +0200

    Started reading opcodes

---
+++ src/interconnect.rs
@@ -0,0 +1,34 @@
+const PIF_ROM_SIZE: usize = 2048;
+
+const RAM_SIZE: usize = 4 * 1024 * 1024;
+
+pub struct Interconnect {
+    pif_rom: Vec<u8>,
+
+    ram: Vec<u16>
+}
+
+impl Interconnect {
+    pub fn new(pif_rom: Vec<u8>) -> Interconnect {
+        Interconnect {
+            pif_rom: pif_rom,
+
+            ram: vec![0; RAM_SIZE]
+        }
+    }
+
+    pub fn read_word(&self, addr: u32) -> u32{
+        //TODO: constants
+        if addr >= 0x1fc0_0000 && addr < 0x1fc0_07c0 {
+            let rel_addr = addr - 0x1fc0_0000;
+            //TODO: check endianess
+            //TODO: Check out byteorder crate
+            ((self.pif_rom[rel_addr as usize] as u32) << 24) | 
+            ((self.pif_rom[(rel_addr+1) as usize] as u32) << 16) | 
+            ((self.pif_rom[(rel_addr+2) as usize] as u32) << 8) | 
+            (self.pif_rom[(rel_addr+3) as usize] as u32)
+        } else{
+            panic!("Unrecognized address: {:#x}", addr);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod n64;
+mod cpu;
+mod interconnect;
+
+use std::env;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let pif_file_name = env::args().nth(1).unwrap();
+    let rom_file_name = env::args().nth(2).unwrap();
+    
+    let pif = read_bin(pif_file_name);
+    let rom = read_bin(rom_file_name);
+
+    let mut n64 = n64::N64::new(pif);
+    n64.power_on_reset();
+    n64.run();
+}
+
+fn read_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = fs::File::open(path).unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf).unwrap();
+    file_buf
 }
